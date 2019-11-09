474ef1a49652db8ce8cd62b78f9c9c87fbbcb291
Jinjin123 <1293813551@qq.com>
2019-11-09 23:16:49 +0800

    catch trump news

---
+++ trump/trump.py
@@ -0,0 +1,63 @@
+#-*- coding:utf-8 -*-
+#python3.6+
+import json
+import os
+import datetime
+import MySQLdb
+import hashlib
+import random
+import requests
+import twint
+
+today = datetime.datetime.now().strftime('%Y-%m-%d')
+
+c = twint.Config()
+c.Username = "realDonaldTrump"
+c.Proxy_type="http"
+c.Proxy_host="localhost"
+c.Proxy_port=3213
+#c.Search = "twint"
+c.Since=today
+c.Output = "trump.json"
+c.Store_json = True
+twint.run.Search(c)
+
+
+url="http://api.fanyi.baidu.com/api/trans/vip/translate"
+appid = '20191105000353104' 
+secretKey = 'w92s1mq62xYvVAW3Ldlo' 
+salt = random.randint(32768, 65536)
+
+def get_tra_res(q,fromLang='en',toLang='zh'):
+    sign = appid + q + str(salt) + secretKey
+    sign = hashlib.md5(sign.encode()).hexdigest()
+    data = {
+        "appid": appid,
+        "q": q,
+        "from": fromLang,
+        "to" : toLang,
+        "salt" : str(salt),
+        "sign" : sign,
+    }
+    res = requests.post(url, data=data)
+    trans_result = json.loads(res.content).get('trans_result')[0].get("dst")
+    return trans_result
+
+
+mq2=MySQLdb.connect(host="192.168.50.100",port=3306,user="root",passwd="zxc123",db="spiderflow",charset='utf8')
+cur = mq2.cursor()
+with open("trump.json","rb") as b:
+    for line in b.readlines():
+        tmp = json.loads(line)
+        sql="select tag from trump where tag = '%s'" % str(tmp["created_at"])
+        row = cur.execute(sql)
+        if row == 0:
+            trs = get_tra_res(tmp["tweet"].split("https")[0])
+            cur.execute('insert into trump (content,tag,time) values (%s,%s,%s)', (trs,str(tmp["created_at"]),(datetime.datetime.strptime(tmp["date"]+" "+tmp["time"],"%Y-%m-%d %H:%M:%S")).strftime("%Y-%m-%d %H:%M:%S")))
+            mq2.commit()
+
+
+
+if (os.path.exists("./trump.json")):
+    os.remove("trump.json")
+
